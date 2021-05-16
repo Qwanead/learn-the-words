732b4dbe08b37295152becfd784441e27a0f4ee6
Rassadnikov Anton <qwanead@protonmail.com>
2021-05-16 06:34:42 +0500

    lesson 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeaderBlock from './components/header-block/header-block';
+
+function App() {
+  return (
+    <HeaderBlock
+      title='Учите слова онлайн'
+      svgLogo
+    />
+  );
+}
+
+export default App;
